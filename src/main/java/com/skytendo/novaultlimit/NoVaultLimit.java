faaf2e733c80e7d70112c986ad90e5bea4285eb9
Tim <82809793+timasshoff@users.noreply.github.com>
2024-09-04 21:45:59 +0200

    Added MOD_ID static field

--- src/main/java/com/skytendo/novaultlimit/NoVaultLimit.java
+++ src/main/java/com/skytendo/novaultlimit/NoVaultLimit.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class NoVaultLimit implements ModInitializer {
+	public static final String MOD_ID = "novaultlimit";
     public static final Logger LOGGER = LoggerFactory.getLogger("novaultlimit");
 
 	@Override
